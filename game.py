cfbfca491b10c9a523f61f7fd2ce9d0bf9e5bbdd
Sallar <106476322+sallar-ba@users.noreply.github.com>
2023-06-11 15:01:31 +0500

    Basic Setup, Added Font, Added Graphics

--- game.py
+++ game.py
@@ -1,23 +1,35 @@
 import pygame
+from sys import exit
+import random
+import math
 
-# Initialize Pygame
+#Initializing the PyGame
 pygame.init()
 
-# Set up the display
-WIDTH = 800
-HEIGHT = 800
+WIDTH = 1000
+HEIGHT = 600
+
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Sum Trail")
+pygame.display.set_caption('SumTrail')
+icon = pygame.image.load("graphics\logo.jpg")
+pygame.display.set_icon(icon)
+clock = pygame.time.Clock()
+font = pygame.font.Font('font\pixeltype.ttf', 50)
 
 
-def game_loop():
-    running = True
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-    pygame.quit()
+sky_surface = pygame.image.load("graphics\sky.jpg").convert()
+text_surface = font.render("Sum: ", False, 'White')
 
 
-# Start the game
-game_loop()
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+    
+    screen.blit(sky_surface, (0, 0))
+    screen.blit(text_surface, (450, 50))
+    
+    pygame.display.update()
+    clock.tick(60)
+        
